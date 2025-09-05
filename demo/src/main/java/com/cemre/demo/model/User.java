14458ba5e31ca2801f2639cab2fbd277cd59ad76
cemresirin <cemre25355@gmail.com>
2025-09-05 15:11:28 +0300

    add UserService Interface

--- demo/src/main/java/com/cemre/demo/model/User.java
+++ demo/src/main/java/com/cemre/demo/model/User.java
@@ -19,4 +19,5 @@ public class User {
     private String username;
     private String email;
     private String password;
+
 }
--- demo/src/main/java/com/cemre/demo/service/UserService.java
+++ demo/src/main/java/com/cemre/demo/service/UserService.java
@@ -7,5 +7,6 @@ public interface UserService {
     User getUser(Long id);
     User updateUser(Long id , User user);
     void deleteUser(Long id);
+
 }
 
